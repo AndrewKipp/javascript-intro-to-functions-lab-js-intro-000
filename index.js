e9c8019be7279a820cd40adb9d1c13b623ada335
Andrew Kipp <andrewkipp1@gmail.com>
2021-03-14 20:11:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function sayHiToGrandma (string) {
 
 function sayHiToGrandma (string) {
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"}}
+    return "YES INDEED!"}
 
 function sayHiToGrandma (string) {
   if (string === "I love you, Grandma.") {
-    return "I love you, too."}}
+    return "I love you, too."}
